754bf275400c1b65d4bc3e53d6c980f1f4e11a98
Friedrich von Never <friedrich@fornever.me>
2015-04-27 22:11:44 +0600

    Initial commit.

---
+++ ImapSlack/Program.cs
@@ -0,0 +1,19 @@
+﻿using AE.Net.Mail;
+
+namespace ImapSlack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string host = "imap.gmail.com";
+			string login = args[0];
+			string password = args[1];
+
+			using (var imap = new ImapClient(host, login, password, AuthMethods.Login, port: 993, secure: true))
+			{
+				var messages = imap.GetMessages(0, 5);
+			}
+		}
+	}
+}
